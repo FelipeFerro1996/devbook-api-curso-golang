a36d519dc98ce07d35c22d927bcd740e02be145a
FelipeFerro1996 <lipe_ferro96@hotmail.com>
2026-09-23 16:56:39 -0300

    feat: adiconando inicio do crud de usuarios e padronizando respostas em json

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,11 +1,44 @@
 package controllers
 
 import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
 func CadastrarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Cadastrando usuario no banco de dados"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositoriosUsuarios := repositorios.NovoRepositoriosDeUsuarios(db)
+	usuario.ID, erro = repositoriosUsuarios.Criar(usuario)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, usuario)
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
