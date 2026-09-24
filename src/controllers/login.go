b29b7cea1229a860cc8ecda463aa3517b0f8cd1f
FelipeFerro1996 <lipe_ferro96@hotmail.com>
2026-09-24 16:13:33 -0300

    feat: adicionando função de login e retornando token na rota

---
+++ src/controllers/login.go
@@ -0,0 +1,56 @@
+package controllers
+
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"api/src/seguranca"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario modelos.Usuario
+	erro = json.Unmarshal(corpoRequisicao, &usuario)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositoriosDeUsuarios(db)
+	UsuarioSalvoNoBanco, erro := repositorio.BuscarPorEmail(usuario.Email)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro := seguranca.VerificarSenha(UsuarioSalvoNoBanco.Senha, usuario.Senha); erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	token, erro := autenticacao.CriarToken(UsuarioSalvoNoBanco.ID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	w.Write([]byte(token))
+
+}
