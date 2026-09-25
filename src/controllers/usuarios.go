6ad592267ebc0ad8ea6b9007236c40b06907a593
FelipeFerro1996 <lipe_ferro96@hotmail.com>
2026-09-25 16:39:39 -0300

    feat: adicionando autenticação com jwt e validação de usuario logado não poder alterar nem deletar outro usuario

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
+	"api/src/autenticacao"
 	"api/src/banco"
 	"api/src/modelos"
 	"api/src/repositorios"
 	"api/src/respostas"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -106,6 +109,19 @@ func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	usuarioDoToken, erro := autenticacao.ExtrairusuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	fmt.Println(usuarioID, usuarioDoToken)
+
+	if usuarioID != usuarioDoToken {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possivel atualizar um usuario que não seja o seu"))
+		return
+	}
+
 	body, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
 		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
@@ -144,6 +160,17 @@ func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
 	if erro != nil {
 		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	usuarioIDNoToken, erro := autenticacao.ExtrairusuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	if usuarioID != usuarioIDNoToken {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possível deletar um usuário que não seja o seu"))
 		return
 	}
 
